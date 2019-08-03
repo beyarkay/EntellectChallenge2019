c7bb0517412ee70ab6084716d19fc0d2b56ff92b
Luc Hayward <luchay10@gmail.com>
2019-08-03 09:11:07 +0200

    Added specification

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
+/*
+ Each task has a cost of 1 shift/unit
+Tasks cost double outside the worm speciality
+Worms can take a break (free shift)
+
+Worms CANNOT start a new task without completing the first
+
+All worms on break = penalty
+
+<-------------->
+
+3 shifts = day
+- Morning (always first)
+- Afternoon
+- Night
+
+D,0,3,0
+Morning 0
+Afternoon 3
+Night 0
+
+Uncompleted tasks move over to next shift
+
+
+ */
 public class Main {
 	public static void main(String[] args) {
 	
